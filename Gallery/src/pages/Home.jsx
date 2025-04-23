1957d82b45bf9297dde611a908a6f73c4bb855a7
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-04-23 14:37:13 +0530

    Add the part of  storage limit and show how much strage is used

--- Gallery/src/pages/Home.jsx
+++ Gallery/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer.jsx";
-import NavBar from "../components/NavBar.jsx";
+import NavBar from "../components/Navbar.jsx";
 import UploadBtn from "../components/UploadBtn.jsx";
 import "../styles/UploadBtn.css";
 import { auth, db } from "../utils/firebase";
@@ -69,15 +69,28 @@ const Home = () => {
       <div
         className="main"
         style={{
-          display: "flex",
-          flexWrap: "wrap",
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
           gap: "10px",
           padding: "1rem 2rem",
+          justifyItems: "center",
         }}
       >
         {images.length > 0 ? (
           images.map((image) => (
-            <div key={image.id} style={{ width: "auto", height: "350px" }}>
+            <div
+              key={image.id}
+              style={{
+                width: "100%",
+                height: "300px",
+                overflow: "hidden",
+                borderRadius: "8px",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                cursor: "pointer",
+                position: "relative",
+                transition: "transform 0.3s ease",
+              }}
+            >
               <img
                 src={image.imageUrl}
                 alt="uploaded"
@@ -86,7 +99,14 @@ const Home = () => {
                   height: "100%",
                   objectFit: "cover",
                   borderRadius: "8px",
+                  transition: "transform 0.3s ease",
                 }}
+                onMouseOver={(e) =>
+                  (e.currentTarget.style.transform = "scale(1.1)")
+                }
+                onMouseOut={(e) =>
+                  (e.currentTarget.style.transform = "scale(1)")
+                }
               />
             </div>
           ))
@@ -94,7 +114,6 @@ const Home = () => {
           <p>No images in this folder yet.</p>
         )}
       </div>
-
       <Footer />
     </div>
   );
